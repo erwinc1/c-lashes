94792f172760239c9061500e74b709a56f76e070
Erwin Cavas <erwin.cavas@rabobank.nl>
2019-10-28 22:07:06 +0100

    Added price table and cleaned default layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Transaction {
+  item: string;
+  cost: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'c-lashes';
+
+export class PriceList {
+  displayedColumns: string[] = ['item', 'cost'];
+  transactions: Transaction[] = [
+    { item: 'Wimpers', cost: 45 },
+    { item: 'Wimpers Extra', cost: 50 },
+    { item: 'Wimpers Super', cost: 55 },
+    { item: 'Wimpers Super Deluxe', cost: 60 },
+    { item: 'Wimpers Ultra', cost: 65 },
+    { item: 'Wimpers Ultra Deluxe', cost: 70 },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from './material-module';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { PriceList } from './app.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [PriceList],
+  declarations: [PriceList],
+  bootstrap: [PriceList],
+  providers: []
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
